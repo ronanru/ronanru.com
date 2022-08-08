7c2072f69be1ffed41ea43dc8709b67550db69d5
Matvey Ryabchikov <35634442+ronanru@users.noreply.github.com>
2022-08-08 21:50:57 +0300

    new welcome window

--- src/lib/components/WelcomeWindow.svelte
+++ src/lib/components/WelcomeWindow.svelte
@@ -17,15 +17,16 @@
   bind:isOpen
   on:close={() => dispatch('close')}
   on:mousedown={() => dispatch('mousedown')}>
-  <div class="flex h-full flex-col items-center justify-center gap-4 text-center">
-    <h1 class="text-2xl font-bold">Welcome to my site! 👋</h1>
-    <p>Hello, my name is Matvey and I'm a full-stack web developer.</p>
-    <p>
-      If you want to play with other programs, click the start menu in the top left corner. <br />
-      If you want to learn more about me, click the button below.
-    </p>
-    <button
-      on:click={() => openWindow('aboutme')}
-      class="rounded-md bg-blue-600 p-4 text-white hover:bg-blue-700">About me</button>
+  <div class="grid place-items-center h-full">
+    <div class="space-y-6 w-full">
+      <h1 class="text-6xl grid font-bold">
+        <span class="bg-gradient-to-tl from-indigo-800 to-blue-600 bg-clip-text text-transparent"
+          >Hi there,</span> I'm Matvey
+      </h1>
+      <p class="text-2xl">I'm a full-stack web developer.</p>
+      <button
+        on:click={() => openWindow('aboutme')}
+        class="rounded-md bg-blue-600 p-4 text-white hover:bg-blue-700 text-lg">About me</button>
+    </div>
   </div>
 </BaseWindow>
