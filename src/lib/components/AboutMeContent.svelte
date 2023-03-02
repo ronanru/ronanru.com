5903c4c531a25f6827a9cf6d6d47b800303ff3e5
Matvey Ryabchikov <35634442+ronanru@users.noreply.github.com>
2023-03-02 21:20:31 +0300

    new about me

--- src/lib/components/AboutMeContent.svelte
+++ src/lib/components/AboutMeContent.svelte
@@ -2,101 +2,89 @@
   import svelteFlipClockImage from '../../assets/projects/svelte-flip-clock.webp';
   import archexplorerImage from '../../assets/projects/archexplorer.webp';
   import myfaqpageImage from '../../assets/projects/myfaqpage.webp';
-  import spworldsImage from '../../assets/projects/spworlds.webp';
   import spdonateImage from '../../assets/projects/spdonate.webp';
   import filmanImage from '../../assets/projects/filman.webp';
-  import { mdiDiscord, mdiGithub, mdiTwitter } from '@mdi/js';
+  import { mdiGithub, mdiTwitter, mdiFileDocument } from '@mdi/js';
   import avatar from '../../assets/avatar.webp';
+  import blogIcon from '../../assets/peppo.webp';
   import Icon from './Icon.svelte';
 </script>
 
 <div class="mx-auto flex max-w-lg flex-col gap-4">
-  <div class="mt-8 flex gap-4 flex-wrap">
+  <h1 class="text-4xl font-bold mt-8 text-center mb-4">Hello, I'm Matvey! 👋</h1>
+  <div class="gap-4 flex-wrap grid grid-cols-3">
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      class="rounded-3xl border-4 border-black hover:bg-black hover:-translate-y-1 translate-y-0 duration-500 hover:text-white transition-all group text-black shadow-lg aspect-square flex flex-col text-lg justify-center items-center"
+      href="https://github.com/ronanru">
+      <div class="group-hover:-rotate-6 group-hover:scale-110 transition-transform duration-500">
+        <Icon icon={mdiGithub} size={4} />
+      </div>
+      RonanRU
+    </a>
+    <!-- svelte-ignore a11y-img-redundant-alt -->
     <img
       src={avatar}
-      class="float-left h-56 w-56 rounded-3xl border border-black shadow-lg"
-      alt="" />
-    <div class="space-y-2">
-      <h1 class="text-2xl font-bold">Hello, I'm Matvey!</h1>
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        class="flex items-center gap-2"
-        href="https://github.com/ronanru">
-        <Icon icon={mdiGithub} />
-        RonanRU
-      </a>
-      <button
-        class="flex items-center gap-2 text-[#5865F2]"
-        on:click={() => navigator.clipboard.writeText('Ronan#2587')}>
-        <Icon icon={mdiDiscord} />
-        Ronan#2587
-      </button>
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        class="flex items-center gap-2 text-[#0088CC]"
-        href="https://t.me/ryabchikovm">
+      class="rounded-3xl border-4 border-black shadow-lg aspect-square hover:-translate-y-1 translate-y-0 duration-500"
+      alt="My profile picture" />
+
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      class="rounded-3xl bg-[#0088CC] border-4 border-[#0088CC] hover:bg-white hover:text-[#0088CC] duration-500 group transition-all hover:-translate-y-1 translate-y-0 text-white shadow-lg aspect-square flex flex-col text-lg justify-center items-center"
+      href="https://t.me/ryabchikovm">
+      <div class="group-hover:-rotate-6 group-hover:scale-110 transition-transform duration-500">
         <Icon
-          icon="M9.78,18.65L10.06,14.42L17.74,7.5C18.08,7.19 17.67,7.04 17.22,7.31L7.74,13.3L3.64,12C2.76,11.75 2.75,11.14 3.84,10.7L19.81,4.54C20.54,4.21 21.24,4.72 20.96,5.84L18.24,18.65C18.05,19.56 17.5,19.78 16.74,19.36L12.6,16.3L10.61,18.23C10.38,18.46 10.19,18.65 9.78,18.65Z" />
-        @ryabchikovm
-      </a>
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        class="flex items-center gap-2 text-[#1DA1F2]"
-        href="https://twitter.com/ryabchikovm">
-        <Icon icon={mdiTwitter} />
-        @ryabchikovm
-      </a>
-    </div>
+          icon="M9.78,18.65L10.06,14.42L17.74,7.5C18.08,7.19 17.67,7.04 17.22,7.31L7.74,13.3L3.64,12C2.76,11.75 2.75,11.14 3.84,10.7L19.81,4.54C20.54,4.21 21.24,4.72 20.96,5.84L18.24,18.65C18.05,19.56 17.5,19.78 16.74,19.36L12.6,16.3L10.61,18.23C10.38,18.46 10.19,18.65 9.78,18.65Z"
+          size={4} />
+      </div>
+      @ryabchikovm
+    </a>
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      class="rounded-3xl bg-neutral-900 border-4 border-neutral-900 hover:bg-white hover:text-neutral-900 duration-500 group transition-all hover:-translate-y-1 translate-y-0 text-white shadow-lg aspect-square flex flex-col text-lg justify-center items-center gap-2"
+      href="https://blog.ronanru.com">
+      <img
+        width="112"
+        height="112"
+        src={blogIcon}
+        class="w-20 h-20 rounded-xl group-hover:-rotate-6 group-hover:scale-110 transition-transform duration-500"
+        alt="Peppo writing in his notepad" />
+      My Blog
+    </a>
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      class="rounded-3xl border-4 border-[#1DA1F2] hover:bg-[#1DA1F2] hover:-translate-y-1 translate-y-0 duration-500 hover:text-white transition-all group text-[#1DA1F2] shadow-lg aspect-square flex flex-col text-lg justify-center items-center"
+      href="https://twitter.com/ryabchikovm">
+      <div class="group-hover:-rotate-6 group-hover:scale-110 transition-transform duration-500">
+        <Icon icon={mdiTwitter} size={4} />
+      </div>
+      @ryabchikovm
+    </a>
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      class="rounded-3xl bg-indigo-800 border-4 border-indigo-800 hover:bg-white hover:text-indigo-800 duration-500 group transition-all hover:-translate-y-1 translate-y-0 text-white shadow-lg aspect-square flex flex-col text-lg justify-center items-center"
+      href="https://resume.ronanru.com/resume.pdf">
+      <div class="group-hover:-rotate-6 group-hover:scale-110 transition-transform duration-500">
+        <Icon icon={mdiFileDocument} size={4} />
+      </div>
+      My Resume
+    </a>
   </div>
-  <p class="indent-4">
-    I'm a young and aspiring full stack developer, focused on building great user experiences on the
-    web. In every project I like to focus my attention on speed and security, making sure every page
-    loads as fast as possible and every endpoint is safe and secure.
-  </p>
-  <a
-    href="https://resume.ronanru.com/resume.pdf"
-    target="_blank"
-    rel="noopener noreferrer"
-    class="rounded-md bg-blue-600 py-2 px-4 text-white hover:bg-blue-700 mx-auto">See my Resume</a>
-  <h1 class="text-2xl font-bold">Cool stuff I've built:</h1>
+  <h1 class="text-4xl font-bold mt-8 mb-4 text-center">Cool stuff I've built:</h1>
   <div class="flex gap-2">
     <img src={myfaqpageImage} height="270" width="480" class="aspect-video w-48 rounded" alt="" />
     <div>
-      <h2 class="text-xl font-bold">
+      <h2 class="text-lg font-bold">
         <a href="https://myfaq.page">My FAQ Page</a>
-        <span class="rounded-full border-2 border-green-800 p-1 text-sm font-normal text-green-800">
-          Backend
-        </span>
-        <span
-          class="rounded-full border-2 border-purple-800 p-1 text-sm font-normal text-purple-800">
-          Frontend
-        </span>
       </h2>
       <p>
         A web app where anyone can create a beautiful FAQ page in minutes. I used new and trendy T3
         stack for this site: Next.js, TypeScript, tRPC, Prisma.
-      </p>
-    </div>
-  </div>
-  <div class="flex gap-2">
-    <img src={spworldsImage} height="270" width="480" class="aspect-video w-48 rounded" alt="" />
-    <div>
-      <h2 class="text-xl font-bold">
-        <a href="https://spworlds.ru">SPWorlds</a>
-        <span class="rounded-full border-2 border-green-800 p-1 text-sm font-normal text-green-800">
-          Backend
-        </span>
-        <span
-          class="rounded-full border-2 border-purple-800 p-1 text-sm font-normal text-purple-800">
-          Frontend
-        </span>
-      </h2>
-      <p>
-        A website for a minecraft server network with payments and social media functionality. The
-        site is installable PWA.
       </p>
     </div>
   </div>
@@ -108,12 +96,8 @@
       class="aspect-video w-48 rounded"
       alt="" />
     <div>
-      <h2 class="text-xl font-bold">
+      <h2 class="text-lg font-bold">
         <a href="https://svelte-flip-clock.ronanru.dev/">Svelte Flip Clock</a>
-        <span
-          class="rounded-full border-2 border-purple-800 p-1 text-sm font-normal text-purple-800">
-          Frontend
-        </span>
       </h2>
       <p>A flip clock component for Svelte (svelte-flip-clock on npm)</p>
     </div>
@@ -126,15 +110,8 @@
       class="aspect-video w-48 rounded"
       alt="" />
     <div>
-      <h2 class="text-xl font-bold">
+      <h2 class="text-lg font-bold">
         <a href="https://archexplorer.ronanru.dev">ArchExplorer</a>
-        <span class="rounded-full border-2 border-green-800 p-1 text-sm font-normal text-green-800">
-          Backend
-        </span>
-        <span
-          class="rounded-full border-2 border-purple-800 p-1 text-sm font-normal text-purple-800">
-          Frontend
-        </span>
       </h2>
       <p>A search engine for Arch Linux packages with minimal client-side JavaScript.</p>
     </div>
@@ -142,7 +119,7 @@
   <div class="flex gap-2">
     <img src={filmanImage} height="270" width="480" class="aspect-video w-48 rounded" alt="" />
     <div>
-      <h2 class="text-xl font-bold">
+      <h2 class="text-lg font-bold">
         <a href="https://github.com/ronanru/filman">Filman</a>
       </h2>
       <p>A TUI file manager for Linux created with Deno and TypeScript with 0 dependencies.</p>
@@ -151,11 +128,8 @@
   <div class="flex gap-2">
     <img src={spdonateImage} height="270" width="480" class="aspect-video w-48 rounded" alt="" />
     <div>
-      <h2 class="text-xl font-bold">
+      <h2 class="text-lg font-bold">
         <a href="https://spdonate.ronanru.dev">SPDonate</a>
-        <span class="rounded-full border-2 border-green-800 p-1 text-sm font-normal text-green-800">
-          Backend
-        </span>
       </h2>
       <p>A website for streamers to collect donations with real-time alerts.</p>
     </div>
--- src/lib/components/Icon.svelte
+++ src/lib/components/Icon.svelte
@@ -3,6 +3,8 @@
     size = 1;
 </script>
 
-<svg class={['h-6 w-6', 'h-8 w-8', 'h-10 w-10', 'h-12 w-12'][size]} viewBox="0 0 24 24">
+<svg
+  class={['h-6 w-6', 'h-8 w-8', 'h-10 w-10', 'h-12 w-12', 'h-20 w-20'][size]}
+  viewBox="0 0 24 24">
   <path fill="currentColor" d={icon} />
 </svg>
