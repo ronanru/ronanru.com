457fe6c43d8c1b84d7130bae2caacbeab971da44
Matvey Ryabchikov <35634442+ronanru@users.noreply.github.com>
2023-04-20 21:19:46 +0300

    ubuntu font

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import './app.css';
+import '@fontsource/ubuntu/latin.css';
 import App from './App.svelte';
 
 const app = new App({
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,13 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
+/** @type import('tailwindcss').Config */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   darkMode: 'class',
   theme: {
+    fontFamily: {
+      sans: ['Ubuntu', ...fontFamily.sans]
+    },
     extend: {}
   },
   plugins: []
